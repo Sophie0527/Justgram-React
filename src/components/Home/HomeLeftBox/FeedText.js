053e6069992a05566f02d1fc3e0ae4fdc5af36be
손소희 <ssh30510044@gmail.com>
2022-09-08 00:31:14 +0900

    Refactor: 코드정리

--- src/components/Home/HomeLeftBox/FeedText.js
+++ src/components/Home/HomeLeftBox/FeedText.js
@@ -12,14 +12,67 @@ import { CustomMediaStyle } from '../../../styles/CustomMediaStyle';
 
 function FeedText(props) {
   const { feeds, setFeeds, num, feed } = props;
-
-  const [like, setLike] = useState(feed.isLiked);
-  const isLiked = () => {
-    setLike(!like);
+  // <피드 좋아요 버튼 구현 1~2>
+  // 1. like(state)에 feed.isLiked:(boolean)데이터 받아와서 보여주기
+  // 2. feed 좋아요 아이콘 클릭 시, 색상과 아이콘 변경
+  const [feedLike, setFeedLike] = useState(feed.isLiked);
+  const feedLikeBtn = () => {
+    setFeedLike(!feedLike);
   };
 
-  const isLikedBtn = a => {
-    if (a.isLiked === true) {
+  // <댓글 구현하기 1~4>
+  // 1. input창의 입력 값을 setState에 담기
+  // 2. feed 당 추가되야할 필요한 정보를 객체로 state 지정
+  const [inputComment, setInputComment] = useState('');
+  const [newComment, setNewComment] = useState({
+    id: '',
+    name: 'Justcode_bootcamp',
+    content: '',
+    isLiked: false,
+  });
+  // 3. input창에 onChange 이벤트 시,
+  //    setState(setInputComment)를 입력값으로 바꾸고
+  //    newComment에서 content 값을 입력값으로 지정하고
+  //    'feed.commentList'배열 데이터에 id값을 추가해 입력된 객체 만들어 담아두기!
+  const onChange = e => {
+    setInputComment(e.target.value);
+    setNewComment({
+      ...newComment,
+      content: e.target.value,
+      id: feed.commentList.length + 1,
+    });
+  };
+  // 4. 엔터 또는 게시 버튼 클릭 시,
+  //    newComment에 담아둔 객체를 feeds 데이터에 추가해주고
+  //    setNewComment로 content의 값을 다시 초기화해주고
+  //    setInputComment로 input창의 글도 초기화해주기!
+  const addNewComment = num => {
+    setFeeds([...feeds], feeds[num].commentList.push(newComment));
+    setNewComment({ ...newComment, content: '' });
+    setInputComment('');
+  };
+
+  // 댓글 input창에 글자가 1자 이상 이라면,
+  // 게시버튼 색상변경 및 클릭가능하고 엔터로 등록가능 조건!
+  const validation = inputComment => {
+    if (inputComment.length < 1) {
+      return false;
+    }
+    return true;
+  };
+  const valid = validation(inputComment);
+
+  // <피드 댓글 좋아요 버튼 구현 1~2>
+  // 1. HeartButton 클릭 시, feeds데이터의 commentList.isLiked의 값이 바뀌도록 함.
+  // 2. isLiked가 true면 => 토마토색+꽉찬하트, false면 검정색+빈하트
+  const reviewLikeBtn = i => {
+    setFeeds(
+      [...feeds],
+      (feeds[num].commentList[i].isLiked = !feeds[num].commentList[i].isLiked)
+    );
+  };
+  const reviewLikeIcon = f => {
+    if (f.isLiked === true) {
       return (
         <FontAwesomeIcon
           color={'tomato'}
@@ -31,66 +84,33 @@ function FeedText(props) {
     return <FontAwesomeIcon color={'black'} icon={faHeart} alt="하트" />;
   };
 
-  const onClickLikeBtn = i => {
-    setFeeds(
-      [...feeds],
-      (feeds[num].commentList[i].isLiked = !feeds[num].commentList[i].isLiked)
-    );
-  };
-
+  // 삭제버튼 클릭 시, feeds데이터의 commentList의 배열 i 인덱스 1개를 제거한다.
   const deleteComment = i => {
     setFeeds([...feeds], feeds[num].commentList.splice(i, 1));
   };
 
-  //
-  const [inputComment, setInputComment] = useState('');
-
-  const [newComment, setNewComment] = useState({
-    id: '',
-    name: 'Justcode_bootcamp',
-    content: '',
-    isLiked: false,
-  });
-
-  const addNewComment = num => {
-    setFeeds([...feeds], feeds[num].commentList.push(newComment));
-    setNewComment({ ...newComment, content: '' });
-    setInputComment('');
-  };
-
-  const validation = inputComment => {
-    if (inputComment.length < 1) {
-      return false;
-    }
-    return true;
-  };
-  const valid = validation(inputComment);
-
-  //
-
-  const [feedContent, setFeedContent] = useState(feed.content.substr(0, 10));
-
-  const [modalOpen, setModalOpen] = useState(true);
-  const closeModal = () => {
-    if (modalOpen) {
-      setModalOpen(false);
+  // feed.content의 12까지 보여주고,
+  // 더보기 버튼 클릭 시,글자전체 보여주고 더보기 버튼 숨기기
+  const [feedContent, setFeedContent] = useState(
+    feed.content.substr(0, 15) + '...'
+  );
+  const [allContentOpen, setAllContentOpen] = useState(true);
+  const showAll = () => {
+    if (allContentOpen) {
+      setAllContentOpen(false);
       setFeedContent(feed.content);
     }
   };
-
-  //
 
   return (
     <Container>
       <IconBox>
         <div>
           <FontAwesomeIcon
-            color={like ? 'tomato' : 'black'}
-            icon={like ? faHeartCircleCheck : faHeart}
+            color={feedLike ? 'tomato' : 'black'}
+            icon={feedLike ? faHeartCircleCheck : faHeart}
             alt="하트"
-            onClick={() => {
-              isLiked();
-            }}
+            onClick={feedLikeBtn}
           />
           <FontAwesomeIcon icon={faMessage} alt="메세지" />
           <FontAwesomeIcon icon={faCircleDown} alt="저장" />
@@ -106,25 +126,26 @@ function FeedText(props) {
       </LikesBox>
       <FeedContents>
         <span>{feeds[num].user.name}</span>
-        <p>{feedContent}</p>
-        {modalOpen ? (
-          <More
-            id="btn"
-            onClick={() => {
-              closeModal();
-            }}
-          >
-            ... 더 보기
-          </More>
-        ) : null}
+        {/* 피드 글의 길이가 15이하이면 '...더보기' 없이 다보여주기 */}
+        {feeds[num].content.length < 16 ? (
+          <p>{feeds[num].content}</p>
+        ) : (
+          <>
+            <p>{feedContent}</p>
+            {allContentOpen ? (
+              <More id="btn" onClick={showAll}>
+                더 보기
+              </More>
+            ) : null}
+          </>
+        )}
       </FeedContents>
-
-      {feeds[num].commentList?.map((a, i) => {
+      {feeds[num].commentList?.map((f, i) => {
         return (
           <ReviewContents key={i}>
             <div>
-              <span>{a.name}</span>
-              <p>{a.content}</p>
+              <span>{f.name}</span>
+              <p>{f.content}</p>
             </div>
             <div>
               <Delete
@@ -134,8 +155,8 @@ function FeedText(props) {
               >
                 삭제
               </Delete>
-              <HeartButton onClick={() => onClickLikeBtn(i)} type="button">
-                {isLikedBtn(a)}
+              <HeartButton onClick={() => reviewLikeBtn(i)} type="button">
+                {reviewLikeIcon(f)}
               </HeartButton>
             </div>
           </ReviewContents>
@@ -147,17 +168,9 @@ function FeedText(props) {
           placeholder="댓글 달기..."
           value={inputComment}
           type="text"
-          onChange={e => {
-            setInputComment(e.target.value);
-
-            setNewComment({
-              ...newComment,
-              content: e.target.value,
-              id: feed.commentList.length + 1,
-            });
-          }}
-          onKeyPress={() => {
-            if (valid) {
+          onChange={onChange}
+          onKeyDown={e => {
+            if (e.keyCode === 13 && valid) {
               addNewComment(num);
             }
           }}
