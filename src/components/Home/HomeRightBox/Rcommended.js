5e328946a31d658d76796c9db304fb197a13a3b1
손소희 <ssh30510044@gmail.com>
2022-09-08 01:24:48 +0900

    Refactor: data 수정 및 추천박스 데이터 필터하고 맵핑하여 보여주기

--- src/components/Home/HomeRightBox/Rcommended.js
+++ src/components/Home/HomeRightBox/Rcommended.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { CustomMediaStyle } from '../../../styles/CustomMediaStyle';
 
 function Rcommended() {
+  // user의 목데이터를 fetch하여 users 배열에 담기
+  // user의 follow가 false인 경우로 filter하고 mapingp하여 보여주기
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/data/user.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setUsers(data);
+      });
+  }, [setUsers]);
+
   // 모두보기 글자 클릭 시 Container의 max-height: 100%로 변경
   const [all, setAll] = useState(false);
   const btnClick = () => {
@@ -20,39 +33,20 @@ function Rcommended() {
         <p onClick={btnClick}>모두보기</p>
       </RcommendedBoxHeader>
       <RcommendedBoxInfoWrap>
-        <RcommendedBoxInfo>
-          <img
-            alt="추천프로필"
-            src="https://photo-cdn2.icons8.com/LqNg9jtI8PjfmQU8ZHl_pBwx8yxeESbTpmbePrbUQKA/rs:fit:1608:1072/wm:1:re:0:0:0.65/wmid:moose/q:98/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5h/c3NldHMvYXNzZXRz/L3NhdGEvb3JpZ2lu/YWwvMTc2LzExMDU1/ZDZlLTQyMWItNGM3/ZC04YjczLWRiZWM0/ZmFiYmFhMS5qcGc.jpg"
-          ></img>
-          <div>
-            <span>unnie_s_2022</span>
-            <p>회원님을 팔로우합니다</p>
-          </div>
-          <button>팔로우</button>
-        </RcommendedBoxInfo>
-        <RcommendedBoxInfo>
-          <img
-            alt="추천프로필"
-            src="https://photo-cdn2.icons8.com/CY-anxzK_EfsUIvPlYT9PSnb7rxEcVHJ8YBquZTbbdI/rs:fit:1606:1072/wm:1:re:0:0:0.65/wmid:moose/q:98/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5h/c3NldHMvYXNzZXRz/L3NhdGEvb3JpZ2lu/YWwvOTI0L2Y5ZTc5/ODgxLTNmZDQtNDVm/Ni05YTRlLTczMzhk/MmQ4OTc4MS5qcGc.jpg"
-          ></img>
-          <div>
-            <span>mizineee</span>
-            <p>Ted_88님 외 2명이 팔로...</p>
-          </div>
-          <button>팔로우</button>
-        </RcommendedBoxInfo>
-        <RcommendedBoxInfo>
-          <img
-            alt="추천프로필"
-            src="https://photo-cdn2.icons8.com/XdKmZyDnJVDgF4L5VIVkHfjzO1y0geJOYGm343_vgXk/rs:fit:715:1072/wm:1:re:0:0:0.65/wmid:moose/q:98/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5h/c3NldHMvYXNzZXRz/L3NhdGEvb3JpZ2lu/YWwvODk1L2RmOWYw/OWE5LTQwZDMtNDkz/ZS1hMzI5LWUyY2Jh/ZTViNDA4ZC5qcGc.jpg"
-          ></img>
-          <div>
-            <span>jeong_1</span>
-            <p>__yum_s님이 팔로우합니다</p>
-          </div>
-          <button>팔로우</button>
-        </RcommendedBoxInfo>
+        {users
+          .filter(user => user.follow === false)
+          .map(user => {
+            return (
+              <RcommendedBoxInfo key={user.id}>
+                <img alt="추천프로필" src={user.image}></img>
+                <div>
+                  <span>{user.userName}</span>
+                  <p>{user.name}</p>
+                </div>
+                <button>팔로우</button>
+              </RcommendedBoxInfo>
+            );
+          })}
       </RcommendedBoxInfoWrap>
     </Container>
   );
@@ -64,7 +58,7 @@ const Container = styled.div`
   align-items: center;
   background-color: white;
   border: 1px solid #dbdbdb;
-  border-radius: 3px;
+  border-radius: 8px;
   padding: 13px 13px;
   max-height: 200px;
   &.active {
@@ -107,7 +101,7 @@ const RcommendedBoxInfo = styled.div`
   display: flex;
   justify-content: left;
   align-items: center;
-  width: 100%;
+  /* width: 100%; */
   button:hover {
     color: #a0a0a0;
   }
@@ -117,23 +111,26 @@ const RcommendedBoxInfo = styled.div`
     object-fit: cover;
     border-radius: 50%;
     border: 1px solid #dbdbdb;
-    margin: 5px 5px 5px 0;
+    margin: 5px;
+    cursor: pointer;
   }
   div {
-    width: 15px;
+    width: 155px;
+    padding-left: 5px;
     span {
       font-size: 15px;
       font-weight: 500;
+      cursor: pointer;
     }
     p {
       font-size: 14px;
       color: #a0a0a0;
       padding-top: 5px;
+      cursor: pointer;
     }
   }
 
   button {
-    margin-left: 150px;
     border: none;
     background-color: white;
     color: #0095f6;
